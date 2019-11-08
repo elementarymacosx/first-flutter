65923d82b5e4464086a338a66d34f8cb10485fd6
Phoo Pwint Aung <p-aung@isgm2.com>
2019-11-08 13:48:15 +0630

    First

--- lib/main.dart
+++ lib/main.dart
@@ -116,10 +116,11 @@ class _MyHomePageState extends State<MyHomePage>
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
+      floatingActionButton: FloatingActionButton
+        (
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: Icon(Icons.add),
+        child: Icon(Icons.cake),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
